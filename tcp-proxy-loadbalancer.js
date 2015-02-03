0ad9b40d72fe0b5e6c9967209f0398f736d09e50
oskmkr <oskmkr@naver.com>
2015-02-03 21:36:59 +0900

    reformatting

--- tcp-proxy-loadbalancer.js
+++ tcp-proxy-loadbalancer.js
@@ -10,30 +10,30 @@ var LOG = require('winston');
 console.log('service start');
 
 var targets = [ {
-	host : 'localhost',
-	port : 5672
+    host : 'localhost',
+    port : 5672
 }, {
-	host : 'localhost',
-	port : 5673
+    host : 'localhost',
+    port : 5673
 }, {
-	host : 'localhost',
-	port : 5674
+    host : 'localhost',
+    port : 5674
 } ];
 
 var nextProxy = function() {
-	var target = targets.shift();
-	targets.push(target);
-	return target;
+    var target = targets.shift();
+    targets.push(target);
+    return target;
 };
 
 var server = net.createServer(function(socket) {
-	var oNextProxy = nextProxy();
+    var oNextProxy = nextProxy();
 
-	console.log("next proxy port is " + oNextProxy.port);
+    console.log("next proxy port is " + oNextProxy.port);
 
-	new TcpProxy().proxy(socket, {
-		target : oNextProxy
-	});
+    new TcpProxy().proxy(socket, {
+	target : oNextProxy
+    });
 });
 
 server.listen(9000);
