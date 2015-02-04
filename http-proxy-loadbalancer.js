c4d28d4ace5d46bb14dc05ef4b1970d5b9551878
oskmkr <oskmkr@naver.com>
2015-02-04 21:41:42 +0900

    reformatting

--- http-proxy-loadbalancer.js
+++ http-proxy-loadbalancer.js
@@ -9,58 +9,58 @@ var http = require('http'), httpProxy = require('http-proxy'), winston = require
 console.log('starting server...');
 
 var targets = [ {
-	host : 'localhost',
-	port : 4000
+    host : 'localhost',
+    port : 4000
 }, {
-	host : 'localhost',
-	port : 5000
+    host : 'localhost',
+    port : 5000
 } ];
 
 var proxy = httpProxy.createProxyServer();
 
 var nextProxy = function() {
-	var target = targets.shift();
-	targets.push(target);
-	return target;
+    var target = targets.shift();
+    targets.push(target);
+    return target;
 };
 
 http.createServer(function(req, res) {
-	console.log('# server port 9000');
+    console.log('# server port 9000');
 
-	var oNextProxy = nextProxy();
+    var oNextProxy = nextProxy();
 
-	winston.log('info', 'forward to ' + oNextProxy.host + ':' + oNextProxy.port);
-	
-	proxy.web(req, res, {
-		target : oNextProxy
-	});
+    winston.log('info', 'forward to ' + oNextProxy.host + ':' + oNextProxy.port);
+
+    proxy.web(req, res, {
+	target : oNextProxy
+    });
 
 }).listen(9000);
 
 /**
- * HTTP server, listening port 5000 
+ * HTTP server, listening port 5000
  */
 http.createServer(function(req, res) {
-	console.log('[listen] server port 5000');
+    console.log('[listen] server port 5000');
 
-	res.writeHead(200, {
-		'Content-Type' : 'text/plain'
-	});
-	res.write('request success\n' + JSON.stringify(req.headers, true, 2));
-	res.end();
+    res.writeHead(200, {
+	'Content-Type' : 'text/plain'
+    });
+    res.write('request success\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
 }).listen(5000);
 
 /**
- * HTTP server, listening port 4000 
+ * HTTP server, listening port 4000
  */
 http.createServer(function(req, res) {
-	console.log('[listen] server port 4000');
+    console.log('[listen] server port 4000');
 
-	res.writeHead(200, {
-		'Content-Type' : 'text/plain'
-	});
-	res.write('request success\n' + JSON.stringify(req.headers, true, 2));
-	res.end();
+    res.writeHead(200, {
+	'Content-Type' : 'text/plain'
+    });
+    res.write('request success\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
 }).listen(4000);
 
 console.log('started server... OK');
